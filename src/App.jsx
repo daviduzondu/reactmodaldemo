255296d9494b1762b3fc6ba3a70c4a79491089ed
daviduzondu <uzondudavid2004@gmail.com>
2023-05-07 15:05:07 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, {useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  const [modal, setModal] = useState([false, undefined])
+
+  return (
+    <>
+      <button onClick={() => setModal([true, "error"])}>Show Error Modal</button>
+      <button onClick={() => setModal([true, "success"])}>Show Sucess Modal</button>
+      {
+        modal[0] && ReactDOM.createPortal(
+          <Modal type={modal[1]} setModal={() => setModal} />,
+          document.getElementById('modal')
+        )
+      }
+    </>
+  )
+}
+
+export default App
+
+
+function Modal({ type, setModal }) {
+  let errorMessage = `This is an error modal.
+  Error modals are used to inform the user that something has gone wrong or some particular operation has failed to execute properly.`
+
+  let successMessage = `This is an success modal.
+  Success modals are used to inform the user that a particular operation has successfully completed.`
+
+  return (
+    <div className='overlay'>
+      <div className='modal'>
+        <div className='modal-title'>{type === "error" ? "⚠️" : "✅"}{type.toUpperCase()}</div>
+        <div className='modal-content'>
+          {type==="error" && errorMessage}
+          {type==="success" && successMessage}
+        </div>
+        <button onClick={setModal(false)}>Close</button>
+      </div>
+    </div>
+
+  )
+}
